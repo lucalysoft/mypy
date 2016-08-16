lucalysoft/mypy

commit c39ba12e68e6d46b4b6fafc5d1e2a7ae84838a47

Fix silent imports/incremental mode/NoneType bug (#2024)
Fixes #2017.

This bug manifests when...

1. You run mypy with silent_imports and incremental enabled against
   a subset of files
2. You re-run mypy with silent_imports and incremental enabled against
   a _slightly different_ subset of files (and end up needing to check
   a previously suppressed file in the process).

The issue appears to be that modules added to the "suppressed" field in
the cache metadata were assumed to also be suppressed in future runs of
mypy. Since the user can change what files they want to recheck, this
was an unsafe assumption.

This commit modifies the graph loading process so that any modules
previously marked as suppressed are manually rechecked again, just in
case.

There are also modifications to how the test suite handles silent_imports
and fixtures in order to pass.

mypy/build.py
@@ -1359,7 +1359,7 @@ def load_graph(sources: List[BuildSource], manager: BuildManager) -> Graph:
     # Collect dependencies.  We go breadth-first.
     while new:
         st = new.popleft()
-        for dep in st.ancestors + st.dependencies:
+        for dep in st.ancestors + st.dependencies + st.suppressed:
             if dep not in graph:
                 try:
                     if dep in st.ancestors:
@@ -1380,6 +1380,11 @@ def load_graph(sources: List[BuildSource], manager: BuildManager) -> Graph:
                     new.append(newst)
             if dep in st.ancestors and dep in graph:
                 graph[dep].child_modules.add(st.id)
+            if dep in graph and dep in st.suppressed:
+                # Previously suppressed file is now visible
+                if dep in st.suppressed:
+                    st.suppressed.remove(dep)
+                    st.dependencies.append(dep)
     for id, g in graph.items():
         if g.has_new_submodules():
             g.parse_file()

test-data/unit/check-incremental.test
@@ -274,3 +274,34 @@ bar(3)
 [out]
 main:1: note: In module imported here:
 tmp/m.py:4: error: Argument 1 to "bar" has incompatible type "int"; expected "str"
+
+[case testIncrementalUnsilencingModule]
+# cmd: mypy -m main package.subpackage.mod2
+# cmd2: mypy -m main package.subpackage.mod1
+# options: silent_imports
+
+[file main.py]
+from package.subpackage.mod1 import Class
+
+def handle(c: Class) -> None:
+    c.some_attribute
+
+[file package/__init__.py]
+# empty
+
+[file package/subpackage/__init__.py]
+# empty
+
+[file package/subpackage/mod1.py]
+import collections # Any previously unloaded package works here
+
+class Class: pass
+
+[file package/subpackage/mod2.py]
+# empty
+
+[builtins fixtures/args.pyi]
+[stale collections, main, package.subpackage.mod1]
+[out]
+tmp/main.py: note: In function "handle":
+tmp/main.py:4: error: "Class" has no attribute "some_attribute"

mypy/test/testcheck.py
@@ -106,10 +106,10 @@ class TypeCheckSuite(DataSuite):
 
     def run_case_once(self, testcase: DataDrivenTestCase, incremental=0) -> None:
         find_module_clear_caches()
-        program_text = '\n'.join(testcase.input)
-        module_name, program_name, program_text = self.parse_module(program_text)
+        original_program_text = '\n'.join(testcase.input)
+        module_data = self.parse_module(original_program_text, incremental)
 
-        options = self.parse_options(program_text)
+        options = self.parse_options(original_program_text)
         options.use_builtins_fixtures = True
         options.python_version = testcase_pyversion(testcase.file, testcase.name)
         set_show_tb(True)  # Show traceback on crash.
@@ -120,8 +120,11 @@ class TypeCheckSuite(DataSuite):
             if incremental == 1:
                 # In run 1, copy program text to program file.
                 output = []
-                with open(program_name, 'w') as f:
-                    f.write(program_text)
+                for module_name, program_path, program_text in module_data:
+                    if module_name == '__main__':
+                        with open(program_path, 'w') as f:
+                            f.write(program_text)
+                        break
             elif incremental == 2:
                 # In run 2, copy *.py.next files to *.py files.
                 for dn, dirs, files in os.walk(os.curdir):
@@ -137,11 +140,13 @@ class TypeCheckSuite(DataSuite):
                             new_time = os.stat(target).st_mtime + 1
                             os.utime(target, times=(new_time, new_time))
 
-            # Always set to none so we're forced to reread program_name
-            program_text = None
-        source = BuildSource(program_name, module_name, program_text)
+        sources = []
+        for module_name, program_path, program_text in module_data:
+            # Always set to none so we're forced to reread the module in incremental mode
+            program_text = None if incremental else program_text
+            sources.append(BuildSource(program_path, module_name, program_text))
         try:
-            res = build.build(sources=[source],
+            res = build.build(sources=sources,
                               options=options,
                               alt_lib_path=test_temp_dir)
             a = res.errors
@@ -159,14 +164,15 @@ class TypeCheckSuite(DataSuite):
                 testcase.file, testcase.line))
 
         if incremental and res:
-            self.verify_cache(module_name, program_name, a, res.manager)
+            if not options.silent_imports:
+                self.verify_cache(module_data, a, res.manager)
             if testcase.expected_stale_modules is not None and incremental == 2:
                 assert_string_arrays_equal(
                     list(sorted(testcase.expected_stale_modules)),
                     list(sorted(res.manager.stale_modules.difference({"__main__"}))),
                     'Set of stale modules does not match expected set')
 
-    def verify_cache(self, module_name: str, program_name: str, a: List[str],
+    def verify_cache(self, module_data: List[Tuple[str, str, str]], a: List[str],
                      manager: build.BuildManager) -> None:
         # There should be valid cache metadata for each module except
         # those in error_paths; for those there should not be.
@@ -176,7 +182,7 @@ class TypeCheckSuite(DataSuite):
         # However build.process_graphs() will ignore A's cache data.
         error_paths = self.find_error_paths(a)
         modules = self.find_module_files()
-        modules.update({module_name: program_name})
+        modules.update({module_name: path for module_name, path, text in module_data})
         missing_paths = self.find_missing_cache_files(modules, manager)
         if missing_paths != error_paths:
             raise AssertionFailure("cache data discrepancy %s != %s" %
@@ -218,28 +224,40 @@ class TypeCheckSuite(DataSuite):
                 missing[id] = path
         return set(missing.values())
 
-    def parse_module(self, program_text: str) -> Tuple[str, str, str]:
+    def parse_module(self, program_text: str, incremental: int = 0) -> List[Tuple[str, str, str]]:
         """Return the module and program names for a test case.
 
-        The default ('__main__') module name can be overridden by
-        using a comment like this in the test case input:
+        Normally, the unit tests will parse the default ('__main__')
+        module and follow all the imports listed there. You can override
+        this behavior and instruct the tests to check multiple modules
+        by using a comment like this in the test case input:
 
-          # cmd: mypy -m foo.bar
+          # cmd: mypy -m foo.bar foo.baz
 
-        Return tuple (main module name, main file name, main program text).
+        Return a list of tuples (module name, file name, program text).
         """
-        m = re.search('# cmd: mypy -m ([a-zA-Z0-9_.]+) *$', program_text, flags=re.MULTILINE)
+        m = re.search('# cmd: mypy -m ([a-zA-Z0-9_. ]+)$', program_text, flags=re.MULTILINE)
+        m2 = re.search('# cmd2: mypy -m ([a-zA-Z0-9_. ]+)$', program_text, flags=re.MULTILINE)
+        if m2 is not None and incremental == 2:
+            # Optionally return a different command if in the second
+            # stage of incremental mode, otherwise default to reusing
+            # the original cmd.
+            m = m2
+
         if m:
             # The test case wants to use a non-default main
             # module. Look up the module and give it as the thing to
             # analyze.
-            module_name = m.group(1)
-            path = build.find_module(module_name, [test_temp_dir])
-            with open(path) as f:
-                program_text = f.read()
-            return m.group(1), path, program_text
+            module_names = m.group(1)
+            out = []
+            for module_name in module_names.split(' '):
+                path = build.find_module(module_name, [test_temp_dir])
+                with open(path) as f:
+                    program_text = f.read()
+                out.append((module_name, path, program_text))
+            return out
         else:
-            return '__main__', 'main', program_text
+            return [('__main__', 'main', program_text)]
 
     def parse_options(self, program_text: str) -> Options:
         options = Options()

test-data/unit/check-modules.test
@@ -726,20 +726,20 @@ tmp/bar.py:1: error: Module has no attribute 'B'
 
 [case testImportSuppressedWhileAlmostSilent]
 # cmd: mypy -m main
-[file main.py]
 # options: silent_imports almost_silent
+[file main.py]
 import mod
 [file mod.py]
 [builtins fixtures/module.pyi]
 [out]
-tmp/main.py:2: note: Import of 'mod' silently ignored
-tmp/main.py:2: note: (Using --silent-imports, module not passed on command line)
-tmp/main.py:2: note: (This note courtesy of --almost-silent)
+tmp/main.py:1: note: Import of 'mod' silently ignored
+tmp/main.py:1: note: (Using --silent-imports, module not passed on command line)
+tmp/main.py:1: note: (This note courtesy of --almost-silent)
 
 [case testAncestorSuppressedWhileAlmostSilent]
 # cmd: mypy -m foo.bar
-[file foo/bar.py]
 # options: silent_imports almost_silent
+[file foo/bar.py]
 [file foo/__init__.py]
 [builtins fixtures/module.pyi]
 [out]
@@ -749,8 +749,8 @@ tmp/foo/bar.py: note: (This note brought to you by --almost-silent)
 
 [case testStubImportNonStubWhileSilent]
 # cmd: mypy -m main
-[file main.py]
 # options: silent_imports
+[file main.py]
 from stub import x # Permitted
 from other import y # Disallowed
 x + '' # Error here
@@ -763,7 +763,7 @@ x = 42
 y = 42
 [builtins fixtures/module.pyi]
 [out]
-tmp/main.py:4: error: Unsupported left operand type for + ("int")
+tmp/main.py:3: error: Unsupported left operand type for + ("int")
 
 [case testSuperclassInImportCycle]
 import a

commit 3176063c1b39e37f00bf6ba705b770d35431f013

Fix regression with type: ignore + silent imports + incremental mode (#2027)
This regression was introduced by #2024.

This commit makes load_graph distinguish between files that were
previously ignored, and files that were silenced due to silent_imports.

mypy/build.py
@@ -1344,6 +1344,7 @@ def load_graph(sources: List[BuildSource], manager: BuildManager) -> Graph:
     # TODO: Consider whether to go depth-first instead.  This may
     # affect the order in which we process files within import cycles.
     new = collections.deque()  # type: collections.deque[State]
+    entry_points = set()  # type: Set[str]
     # Seed the graph with the initial root sources.
     for bs in sources:
         try:
@@ -1356,11 +1357,16 @@ def load_graph(sources: List[BuildSource], manager: BuildManager) -> Graph:
             manager.errors.raise_error()
         graph[st.id] = st
         new.append(st)
+        entry_points.add(bs.module)
     # Collect dependencies.  We go breadth-first.
     while new:
         st = new.popleft()
         for dep in st.ancestors + st.dependencies + st.suppressed:
-            if dep not in graph:
+            # We don't want to recheck imports marked with '# type: ignore'
+            # so we ignore any suppressed module not explicitly re-included
+            # from the command line.
+            ignored = dep in st.suppressed and dep not in entry_points
+            if dep not in graph and not ignored:
                 try:
                     if dep in st.ancestors:
                         # TODO: Why not 'if dep not in st.dependencies' ?

test-data/unit/check-incremental.test
@@ -305,3 +305,42 @@ class Class: pass
 [out]
 tmp/main.py: note: In function "handle":
 tmp/main.py:4: error: "Class" has no attribute "some_attribute"
+
+[case testIncrementalWithIgnores]
+import foo # type: ignore
+
+[stale]
+[out]
+
+[case testIncrementalWithSilentImportsAndIgnore]
+# cmd: mypy -m main b
+# cmd2: mypy -m main c c.submodule
+# options: silent_imports
+
+[file main.py]
+import a  # type: ignore
+import b
+import c
+
+a.A().foo()
+b.B().foo()
+c.C().foo()
+
+[file b.py]
+class B:
+    def foo(self) -> None: pass
+
+[file b.py.next]
+
+[file c/__init__.py]
+class C: pass
+
+[file c/submodule.py]
+val = 3  # type: int
+val = "foo"
+
+[builtins fixtures/module_all.pyi]
+[stale main, c, c.submodule]
+[out]
+tmp/c/submodule.py:2: error: Incompatible types in assignment (expression has type "str", variable has type "int")
+tmp/main.py:7: error: "C" has no attribute "foo"