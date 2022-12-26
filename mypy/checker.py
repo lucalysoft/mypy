lucalysoft/mypy

commit cd03cfde405a3004cb5bac4276d112e83e8e8d00

Add a short note when an error may be fixed by adding an await (#12958)
I only cover few most common situations. If this will show to be useful, we can expand the scope of this note.

mypy/checker.py
@@ -12,7 +12,7 @@ from typing import (
 from typing_extensions import Final, TypeAlias as _TypeAlias
 
 from mypy.backports import nullcontext
-from mypy.errors import Errors, report_internal_error
+from mypy.errors import Errors, report_internal_error, ErrorWatcher
 from mypy.nodes import (
     SymbolTable, Statement, MypyFile, Var, Expression, Lvalue, Node,
     OverloadedFuncDef, FuncDef, FuncItem, FuncBase, TypeInfo,
@@ -38,7 +38,7 @@ from mypy.types import (
     is_named_instance, union_items, TypeQuery, LiteralType,
     is_optional, remove_optional, TypeTranslator, StarType, get_proper_type, ProperType,
     get_proper_types, is_literal_type, TypeAliasType, TypeGuardedType, ParamSpecType,
-    OVERLOAD_NAMES,
+    OVERLOAD_NAMES, UnboundType
 )
 from mypy.sametypes import is_same_type
 from mypy.messages import (
@@ -275,6 +275,10 @@ class TypeChecker(NodeVisitor[None], CheckerPluginInterface):
         # NOTE: we use the context manager to avoid "threading" an additional `is_final_def`
         # argument through various `checker` and `checkmember` functions.
         self._is_final_def = False
+
+        # This flag is set when we run type-check or attribute access check for the purpose
+        # of giving a note on possibly missing "await". It is used to avoid infinite recursion.
+        self.checking_missing_await = False
 
     @property
     def type_context(self) -> List[Optional[Type]]:
@@ -5285,7 +5289,53 @@ class TypeChecker(NodeVisitor[None], CheckerPluginInterface):
                 call = find_member('__call__', supertype, subtype, is_operator=True)
                 assert call is not None
                 self.msg.note_call(supertype, call, context, code=code)
+        self.check_possible_missing_await(subtype, supertype, context)
         return False
+
+    def get_precise_awaitable_type(self, typ: Type, local_errors: ErrorWatcher) -> Optional[Type]:
+        """If type implements Awaitable[X] with non-Any X, return X.
+
+        In all other cases return None. This method must be called in context
+        of local_errors.
+        """
+        if isinstance(get_proper_type(typ), PartialType):
+            # Partial types are special, ignore them here.
+            return None
+        try:
+            aw_type = self.expr_checker.check_awaitable_expr(
+                typ, Context(), '', ignore_binder=True
+            )
+        except KeyError:
+            # This is a hack to speed up tests by not including Awaitable in all typing stubs.
+            return None
+        if local_errors.has_new_errors():
+            return None
+        if isinstance(get_proper_type(aw_type), (AnyType, UnboundType)):
+            return None
+        return aw_type
+
+    @contextmanager
+    def checking_await_set(self) -> Iterator[None]:
+        self.checking_missing_await = True
+        try:
+            yield
+        finally:
+            self.checking_missing_await = False
+
+    def check_possible_missing_await(
+            self, subtype: Type, supertype: Type, context: Context
+    ) -> None:
+        """Check if the given type becomes a subtype when awaited."""
+        if self.checking_missing_await:
+            # Avoid infinite recursion.
+            return
+        with self.checking_await_set(), self.msg.filter_errors() as local_errors:
+            aw_type = self.get_precise_awaitable_type(subtype, local_errors)
+            if aw_type is None:
+                return
+            if not self.check_subtype(aw_type, supertype, context):
+                return
+        self.msg.possible_missing_await(context)
 
     def contains_none(self, t: Type) -> bool:
         t = get_proper_type(t)

mypy/checkexpr.py
@@ -1573,6 +1573,7 @@ class ExpressionChecker(ExpressionVisitor[Type]):
                                                   outer_context=outer_context)
             self.msg.incompatible_argument_note(original_caller_type, callee_type, context,
                                                 code=code)
+            self.chk.check_possible_missing_await(caller_type, callee_type, context)
 
     def check_overload_call(self,
                             callee: Overloaded,
@@ -4119,7 +4120,9 @@ class ExpressionChecker(ExpressionVisitor[Type]):
             self.chk.msg.does_not_return_value(None, e)
         return ret
 
-    def check_awaitable_expr(self, t: Type, ctx: Context, msg: Union[str, ErrorMessage]) -> Type:
+    def check_awaitable_expr(
+            self, t: Type, ctx: Context, msg: Union[str, ErrorMessage], ignore_binder: bool = False
+    ) -> Type:
         """Check the argument to `await` and extract the type of value.
 
         Also used by `async for` and `async with`.
@@ -4131,7 +4134,11 @@ class ExpressionChecker(ExpressionVisitor[Type]):
             generator = self.check_method_call_by_name('__await__', t, [], [], ctx)[0]
             ret_type = self.chk.get_generator_return_type(generator, False)
             ret_type = get_proper_type(ret_type)
-            if isinstance(ret_type, UninhabitedType) and not ret_type.ambiguous:
+            if (
+                not ignore_binder
+                and isinstance(ret_type, UninhabitedType)
+                and not ret_type.ambiguous
+            ):
                 self.chk.binder.unreachable()
             return ret_type
 

mypy/checkmember.py
@@ -171,7 +171,38 @@ def _analyze_member_access(name: str,
         return AnyType(TypeOfAny.from_error)
     if mx.chk.should_suppress_optional_error([typ]):
         return AnyType(TypeOfAny.from_error)
-    return mx.msg.has_no_attr(mx.original_type, typ, name, mx.context, mx.module_symbol_table)
+    return report_missing_attribute(mx.original_type, typ, name, mx)
+
+
+def may_be_awaitable_attribute(
+    name: str,
+    typ: Type,
+    mx: MemberContext,
+    override_info: Optional[TypeInfo] = None
+) -> bool:
+    """Check if the given type has the attribute when awaited."""
+    if mx.chk.checking_missing_await:
+        # Avoid infinite recursion.
+        return False
+    with mx.chk.checking_await_set(), mx.msg.filter_errors() as local_errors:
+        aw_type = mx.chk.get_precise_awaitable_type(typ, local_errors)
+        if aw_type is None:
+            return False
+        _ = _analyze_member_access(name, aw_type, mx, override_info)
+        return not local_errors.has_new_errors()
+
+
+def report_missing_attribute(
+    original_type: Type,
+    typ: Type,
+    name: str,
+    mx: MemberContext,
+    override_info: Optional[TypeInfo] = None
+) -> Type:
+    res_type = mx.msg.has_no_attr(original_type, typ, name, mx.context, mx.module_symbol_table)
+    if may_be_awaitable_attribute(name, typ, mx, override_info):
+        mx.msg.possible_missing_await(mx.context)
+    return res_type
 
 
 # The several functions that follow implement analyze_member_access for various
@@ -438,9 +469,7 @@ def analyze_member_var_access(name: str,
     else:
         if mx.chk and mx.chk.should_suppress_optional_error([itype]):
             return AnyType(TypeOfAny.from_error)
-        return mx.msg.has_no_attr(
-            mx.original_type, itype, name, mx.context, mx.module_symbol_table
-        )
+        return report_missing_attribute(mx.original_type, itype, name, mx)
 
 
 def check_final_member(name: str, info: TypeInfo, msg: MessageBuilder, ctx: Context) -> None:
@@ -851,9 +880,7 @@ def analyze_enum_class_attribute_access(itype: Instance,
                                         ) -> Optional[Type]:
     # Skip these since Enum will remove it
     if name in ENUM_REMOVED_PROPS:
-        return mx.msg.has_no_attr(
-            mx.original_type, itype, name, mx.context, mx.module_symbol_table
-        )
+        return report_missing_attribute(mx.original_type, itype, name, mx)
     # For other names surrendered by underscores, we don't make them Enum members
     if name.startswith('__') and name.endswith("__") and name.replace('_', '') != '':
         return None

mypy/messages.py
@@ -806,6 +806,9 @@ class MessageBuilder:
     def type_not_iterable(self, type: Type, context: Context) -> None:
         self.fail(f'{format_type(type)} object is not iterable', context)
 
+    def possible_missing_await(self, context: Context) -> None:
+        self.note('Maybe you forgot to use "await"?', context)
+
     def incompatible_operator_assignment(self, op: str,
                                          context: Context) -> None:
         self.fail(f'Result type of {op} incompatible in assignment',

test-data/unit/check-async-await.test
@@ -865,6 +865,60 @@ async with C() as x: # E: "async with" outside async function
 [builtins fixtures/async_await.pyi]
 [typing fixtures/typing-async.pyi]
 
+[case testAwaitMissingNote]
+# flags: --python-version 3.7
+from typing import Generic, TypeVar, Generator, Any, Awaitable, Type
+
+class C:
+    x: int
+class D(C): ...
+
+async def foo() -> D: ...
+def g(x: C) -> None: ...
+
+T = TypeVar("T")
+class Custom(Generic[T]):
+    def __await__(self) -> Generator[Any, Any, T]: ...
+
+class Sub(Custom[T]): ...
+
+async def test(x: Sub[D], tx: Type[Sub[D]]) -> None:
+    foo().x  # E: "Coroutine[Any, Any, D]" has no attribute "x" \
+             # N: Maybe you forgot to use "await"?
+    (await foo()).x
+    foo().bad  # E: "Coroutine[Any, Any, D]" has no attribute "bad"
+
+    g(foo())  # E: Argument 1 to "g" has incompatible type "Coroutine[Any, Any, D]"; expected "C" \
+              # N: Maybe you forgot to use "await"?
+    g(await foo())
+    unknown: Awaitable[Any]
+    g(unknown)  # E: Argument 1 to "g" has incompatible type "Awaitable[Any]"; expected "C"
+
+    x.x  # E: "Sub[D]" has no attribute "x" \
+         # N: Maybe you forgot to use "await"?
+    (await x).x
+    x.bad  # E: "Sub[D]" has no attribute "bad"
+
+    a: C = x  # E: Incompatible types in assignment (expression has type "Sub[D]", variable has type "C") \
+              # N: Maybe you forgot to use "await"?
+    b: C = await x
+    unknown2: Awaitable[Any]
+    d: C = unknown2  # E: Incompatible types in assignment (expression has type "Awaitable[Any]", variable has type "C")
+
+    # The notes are not show for Type[...] (because awaiting them will not work)
+    tx.x  # E: "Type[Sub[D]]" has no attribute "x"
+    a2: C = tx  # E: Incompatible types in assignment (expression has type "Type[Sub[D]]", variable has type "C")
+
+class F:
+    def __await__(self: T) -> Generator[Any, Any, T]: ...
+class G(F): ...
+
+# This should not crash.
+x: int = G()  # E: Incompatible types in assignment (expression has type "G", variable has type "int")
+
+[builtins fixtures/async_await.pyi]
+[typing fixtures/typing-async.pyi]
+
 [case testAsyncGeneratorExpressionAwait]
 from typing import AsyncGenerator
 
@@ -874,4 +928,5 @@ async def f() -> AsyncGenerator[int, None]:
 
     return (await g(x) for x in [1, 2, 3])
 
+[builtins fixtures/async_await.pyi]
 [typing fixtures/typing-async.pyi]

test-data/unit/deps.test
@@ -872,6 +872,8 @@ c.y  # type: ignore
 <b.c.y> -> m
 <b.c> -> m
 <b> -> m
+<types.ModuleType.__await__> -> <types.ModuleType>
+<types.ModuleType> -> typing.Awaitable
 
 [case testIgnoredMissingInstanceAttribute]
 from a import C
@@ -879,10 +881,11 @@ C().x  # type: ignore
 [file a.py]
 class C: pass
 [out]
+<a.C.__await__> -> <a.C>
 <a.C.__init__> -> m
 <a.C.__new__> -> m
 <a.C.x> -> m
-<a.C> -> m
+<a.C> -> m, typing.Awaitable
 <a> -> m
 
 [case testIgnoredMissingClassAttribute]

test-data/unit/pythoneval-asyncio.test
@@ -429,6 +429,7 @@ loop.run_until_complete(h())
 loop.close()
 [out]
 _program.py:16: error: Incompatible return value type (got "Future[Future[int]]", expected "Future[int]")
+_program.py:16: note: Maybe you forgot to use "await"?
 
 [case testErrorAssignmentDifferentType]
 import typing

commit 5317db9cde54499be83c3c5de019e8fb043c4dd6

Avoid slow error message logic if errors not shown to user (#14336)
This helps with await-related errors introduced in #12958, in
particular, which are expensive to generate. If errors are ignored (e.g.
in third-party libraries) or we don't care about the error message, use
simpler error message logic. We also often filter out error messages
temporarily, so any effort in constructing a nice error message is
wasted.

We could skip even more logic, but this should cover many of the
important code paths.

This speeds up self check by about 2%.

mypy/checker.py
@@ -5936,6 +5936,10 @@ class TypeChecker(NodeVisitor[None], CheckerPluginInterface):
         if isinstance(msg, str):
             msg = ErrorMessage(msg, code=code)
 
+        if self.msg.prefer_simple_messages():
+            self.fail(msg, context)  # Fast path -- skip all fancy logic
+            return False
+
         orig_subtype = subtype
         subtype = get_proper_type(subtype)
         orig_supertype = supertype

mypy/checkexpr.py
@@ -2157,7 +2157,8 @@ class ExpressionChecker(ExpressionVisitor[Type]):
             self.msg.incompatible_argument_note(
                 original_caller_type, callee_type, context, code=code
             )
-            self.chk.check_possible_missing_await(caller_type, callee_type, context)
+            if not self.msg.prefer_simple_messages():
+                self.chk.check_possible_missing_await(caller_type, callee_type, context)
 
     def check_overload_call(
         self,

mypy/checkmember.py
@@ -272,8 +272,9 @@ def report_missing_attribute(
     override_info: TypeInfo | None = None,
 ) -> Type:
     res_type = mx.msg.has_no_attr(original_type, typ, name, mx.context, mx.module_symbol_table)
-    if may_be_awaitable_attribute(name, typ, mx, override_info):
-        mx.msg.possible_missing_await(mx.context)
+    if not mx.msg.prefer_simple_messages():
+        if may_be_awaitable_attribute(name, typ, mx, override_info):
+            mx.msg.possible_missing_await(mx.context)
     return res_type
 
 

mypy/errors.py
@@ -737,6 +737,24 @@ class Errors:
         """Are there any errors for the given file?"""
         return file in self.error_info_map
 
+    def prefer_simple_messages(self) -> bool:
+        """Should we generate simple/fast error messages?
+
+        Return True if errors are not shown to user, i.e. errors are ignored
+        or they are collected for internal use only.
+
+        If True, we should prefer to generate a simple message quickly.
+        All normal errors should still be reported.
+        """
+        if self.file in self.ignored_files:
+            # Errors ignored, so no point generating fancy messages
+            return True
+        for _watcher in self._watchers:
+            if _watcher._filter is True and _watcher._filtered is None:
+                # Errors are filtered
+                return True
+        return False
+
     def raise_error(self, use_stdout: bool = True) -> NoReturn:
         """Raise a CompileError with the generated messages.
 

mypy/messages.py
@@ -191,6 +191,14 @@ class MessageBuilder:
     def are_type_names_disabled(self) -> bool:
         return len(self._disable_type_names) > 0 and self._disable_type_names[-1]
 
+    def prefer_simple_messages(self) -> bool:
+        """Should we generate simple/fast error messages?
+
+        If errors aren't shown to the user, we don't want to waste cyles producing
+        complex error messages.
+        """
+        return self.errors.prefer_simple_messages()
+
     def report(
         self,
         msg: str,
@@ -685,64 +693,69 @@ class MessageBuilder:
                 actual_type_str, expected_type_str
             )
         else:
-            try:
-                expected_type = callee.arg_types[m - 1]
-            except IndexError:  # Varargs callees
-                expected_type = callee.arg_types[-1]
-            arg_type_str, expected_type_str = format_type_distinctly(
-                arg_type, expected_type, bare=True
-            )
-            if arg_kind == ARG_STAR:
-                arg_type_str = "*" + arg_type_str
-            elif arg_kind == ARG_STAR2:
-                arg_type_str = "**" + arg_type_str
-
-            # For function calls with keyword arguments, display the argument name rather than the
-            # number.
-            arg_label = str(n)
-            if isinstance(outer_context, CallExpr) and len(outer_context.arg_names) >= n:
-                arg_name = outer_context.arg_names[n - 1]
-                if arg_name is not None:
-                    arg_label = f'"{arg_name}"'
-            if (
-                arg_kind == ARG_STAR2
-                and isinstance(arg_type, TypedDictType)
-                and m <= len(callee.arg_names)
-                and callee.arg_names[m - 1] is not None
-                and callee.arg_kinds[m - 1] != ARG_STAR2
-            ):
-                arg_name = callee.arg_names[m - 1]
-                assert arg_name is not None
-                arg_type_str, expected_type_str = format_type_distinctly(
-                    arg_type.items[arg_name], expected_type, bare=True
-                )
-                arg_label = f'"{arg_name}"'
-            if isinstance(outer_context, IndexExpr) and isinstance(outer_context.index, StrExpr):
-                msg = 'Value of "{}" has incompatible type {}; expected {}'.format(
-                    outer_context.index.value,
-                    quote_type_string(arg_type_str),
-                    quote_type_string(expected_type_str),
-                )
+            if self.prefer_simple_messages():
+                msg = "Argument has incompatible type"
             else:
-                msg = "Argument {} {}has incompatible type {}; expected {}".format(
-                    arg_label,
-                    target,
-                    quote_type_string(arg_type_str),
-                    quote_type_string(expected_type_str),
+                try:
+                    expected_type = callee.arg_types[m - 1]
+                except IndexError:  # Varargs callees
+                    expected_type = callee.arg_types[-1]
+                arg_type_str, expected_type_str = format_type_distinctly(
+                    arg_type, expected_type, bare=True
                 )
+                if arg_kind == ARG_STAR:
+                    arg_type_str = "*" + arg_type_str
+                elif arg_kind == ARG_STAR2:
+                    arg_type_str = "**" + arg_type_str
+
+                # For function calls with keyword arguments, display the argument name rather
+                # than the number.
+                arg_label = str(n)
+                if isinstance(outer_context, CallExpr) and len(outer_context.arg_names) >= n:
+                    arg_name = outer_context.arg_names[n - 1]
+                    if arg_name is not None:
+                        arg_label = f'"{arg_name}"'
+                if (
+                    arg_kind == ARG_STAR2
+                    and isinstance(arg_type, TypedDictType)
+                    and m <= len(callee.arg_names)
+                    and callee.arg_names[m - 1] is not None
+                    and callee.arg_kinds[m - 1] != ARG_STAR2
+                ):
+                    arg_name = callee.arg_names[m - 1]
+                    assert arg_name is not None
+                    arg_type_str, expected_type_str = format_type_distinctly(
+                        arg_type.items[arg_name], expected_type, bare=True
+                    )
+                    arg_label = f'"{arg_name}"'
+                if isinstance(outer_context, IndexExpr) and isinstance(
+                    outer_context.index, StrExpr
+                ):
+                    msg = 'Value of "{}" has incompatible type {}; expected {}'.format(
+                        outer_context.index.value,
+                        quote_type_string(arg_type_str),
+                        quote_type_string(expected_type_str),
+                    )
+                else:
+                    msg = "Argument {} {}has incompatible type {}; expected {}".format(
+                        arg_label,
+                        target,
+                        quote_type_string(arg_type_str),
+                        quote_type_string(expected_type_str),
+                    )
+                expected_type = get_proper_type(expected_type)
+                if isinstance(expected_type, UnionType):
+                    expected_types = list(expected_type.items)
+                else:
+                    expected_types = [expected_type]
+                for type in get_proper_types(expected_types):
+                    if isinstance(arg_type, Instance) and isinstance(type, Instance):
+                        notes = append_invariance_notes(notes, arg_type, type)
             object_type = get_proper_type(object_type)
             if isinstance(object_type, TypedDictType):
                 code = codes.TYPEDDICT_ITEM
             else:
                 code = codes.ARG_TYPE
-            expected_type = get_proper_type(expected_type)
-            if isinstance(expected_type, UnionType):
-                expected_types = list(expected_type.items)
-            else:
-                expected_types = [expected_type]
-            for type in get_proper_types(expected_types):
-                if isinstance(arg_type, Instance) and isinstance(type, Instance):
-                    notes = append_invariance_notes(notes, arg_type, type)
         self.fail(msg, context, code=code)
         if notes:
             for note_msg in notes:
@@ -756,6 +769,8 @@ class MessageBuilder:
         context: Context,
         code: ErrorCode | None,
     ) -> None:
+        if self.prefer_simple_messages():
+            return
         if isinstance(
             original_caller_type, (Instance, TupleType, TypedDictType, TypeType, CallableType)
         ):
@@ -832,7 +847,9 @@ class MessageBuilder:
     def too_few_arguments(
         self, callee: CallableType, context: Context, argument_names: Sequence[str | None] | None
     ) -> None:
-        if argument_names is not None:
+        if self.prefer_simple_messages():
+            msg = "Too few arguments"
+        elif argument_names is not None:
             num_positional_args = sum(k is None for k in argument_names)
             arguments_left = callee.arg_names[num_positional_args : callee.min_args]
             diff = [k for k in arguments_left if k not in argument_names]
@@ -856,7 +873,10 @@ class MessageBuilder:
         self.fail(msg, context, code=codes.CALL_ARG)
 
     def too_many_arguments(self, callee: CallableType, context: Context) -> None:
-        msg = "Too many arguments" + for_function(callee)
+        if self.prefer_simple_messages():
+            msg = "Too many arguments"
+        else:
+            msg = "Too many arguments" + for_function(callee)
         self.fail(msg, context, code=codes.CALL_ARG)
         self.maybe_note_about_special_args(callee, context)
 
@@ -874,11 +894,16 @@ class MessageBuilder:
         self.fail(msg, context)
 
     def too_many_positional_arguments(self, callee: CallableType, context: Context) -> None:
-        msg = "Too many positional arguments" + for_function(callee)
+        if self.prefer_simple_messages():
+            msg = "Too many positional arguments"
+        else:
+            msg = "Too many positional arguments" + for_function(callee)
         self.fail(msg, context)
         self.maybe_note_about_special_args(callee, context)
 
     def maybe_note_about_special_args(self, callee: CallableType, context: Context) -> None:
+        if self.prefer_simple_messages():
+            return
         # https://github.com/python/mypy/issues/11309
         first_arg = callee.def_extras.get("first_arg")
         if first_arg and first_arg not in {"self", "cls", "mcs"}: